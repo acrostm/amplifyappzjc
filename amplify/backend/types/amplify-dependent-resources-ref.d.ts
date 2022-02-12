db4fa978e9f6ff43db958bbc0ff5895e41b1e4b7
Jiachen Zhang <39896406+acrostm@users.noreply.github.com>
2022-02-12 22:21:24 +0800

    commented aws-exports.js from gitignore

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,12 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyappzjc": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
